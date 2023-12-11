be848aa46c41abc2f034e8a9d81be2be81598fe5
SillyMonsta <pnk.david@gmail.com>
2023-12-11 22:20:36 +0300

    v 0.011, первый тест работы с кронтабом, +stop_tbot.py, +events_extraction.py (для запуска процесса отдельно от кронтаба вручную)

--- stop_tbot.py
+++ stop_tbot.py
@@ -3,6 +3,6 @@ import os
 import signal
 import data2sql
 
-pid_to_kill = sql2data.pid_from_sql('stream_connection')
+pid_to_kill = int(sql2data.pid_from_sql('stream_connection'))
 os.kill(pid_to_kill, signal.SIGINT)
 data2sql.update_pid('stream_connection', '')
