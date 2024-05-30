eb47f4a01c69168fb6a77464c2d9dc7c56a396d5
SillyMonsta <pnk.david@gmail.com>
2024-05-30 10:25:17 +0300

    v 0.3.2 сделал явный int для количества лотов в сделке при постановке ордера

--- telegram.py
+++ telegram.py
@@ -138,7 +138,7 @@ def handle_message(message):
     # если сообщения от нужного пользователя
     if user_id == 1138331624:
         notice = 'Ошибка ввода.\nДоступные команды:' \
-                 '\n\n1.Переписать значение в analyzed_shares:\nupdate [ticker] [buy] [fast_buy] [sell]' \
+                 '\n\n1.Переписать значение в analyzed_shares:\nupdate [ticker] [buy] [fast_buy] [sell] [vol] [req_vol]' \
                  '\n\n2.Получить данные по акции из analyzed_shares:\nget [ticker]' \
                  '\n\n3.Получить последние строки из events_list:\nevents [row_count]' \
                  '\n\n3.Получить последние строки из orders:\norders [row_count]' \
