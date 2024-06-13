bda441fa2a9f413622dca59aee28e66cb57b4aa6
SillyMonsta <pnk.david@gmail.com>
2024-06-13 12:38:36 +0300

    v 0.4 изменил: sells > ave_sell * 3 buys > ave_buy * 3

--- action.py
+++ action.py
@@ -320,12 +320,11 @@ def calculate_ave_trades(figi, ticker):
         sum_buy = vol_buys
     data2sql.ave_trades2sql([(figi, ticker, ave_sell, ave_buy, sum_sell, sum_buy)])
 
-    if sells > ave_sell*5:
+    if sells > ave_sell * 3:
         write2file.write(str(datetime.datetime.now())[:19] + '  ' + str(ticker) + '  LOT_SELLS  ' + str(price), 'log.txt')
-    if buys > ave_buy*5:
+    if buys > ave_buy * 3:
         write2file.write(str(datetime.datetime.now())[:19] + '  ' + str(ticker) + '  LOT_BUYS  ' + str(price), 'log.txt')
 
-    print(price)
     return
 
 
